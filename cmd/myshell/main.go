c6467d9d4298c61da557a6d691024f470d4dc881
tzgfryziu <patryk.fryzlewicz@gmail.com>
2025-01-22 19:03:02 +0100

    stage 3 passssss

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,13 +8,15 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		command = strings.TrimSpace(command) + ": command not found"
+		fmt.Println(command)
 	}
-	command = strings.TrimSpace(command) + ": command not found"
-	fmt.Println(command)
 }
