783814371de3aa54bf3427cee7b633c6f5d43402
tzgfryziu <patryk.fryzlewicz@gmail.com>
2025-01-26 16:49:37 +0100

    xxx

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"slices"
 	"strconv"
 	"strings"
@@ -36,32 +37,62 @@ func handleCommand(command string, args []string) {
 
 	switch command {
 	case "exit":
-		if len(args) < 1 {
-			fmt.Println("Not enough arguments!")
-			os.Exit(1)
-		}
-		exitCode, err := strconv.Atoi(args[0])
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		os.Exit(exitCode)
+		exit(args)
 	case "echo":
-		for _, s := range args {
-			fmt.Print(s + " ")
-		}
-		fmt.Print("\n")
+		echo(args)
 	case "type":
-		if slices.Contains(BUILTINS, args[0]) {
-			fmt.Println(args[0] + " is a shell builtin")
-		} else {
-			fmt.Println(args[0] + ": not found")
-		}
-
+		type_(args)
 	default:
 		fmt.Println(command + ": command not found")
 
 	}
 
+}
+
+func type_(args []string) {
+	if len(args) < 1 {
+		fmt.Println(args[0] + ": not found")
+	}
+
+	var found bool
+
+	if slices.Contains(BUILTINS, args[0]) {
+		found = true
+		fmt.Println(args[0] + " is a shell builtin")
+	}
+	paths := strings.Split(os.Getenv("PATH"), ";")
+	for _, path := range paths {
+		fp := filepath.Join(path, args[0])
+		_, err := os.Stat(fp)
+		if err == nil {
+			found = true
+			fmt.Println(args[0] + " is " + fp)
+		}
+	}
+	if !found {
+		fmt.Println(args[0] + ": not found")
+	}
+
+}
+
+func echo(args []string) {
+	for _, s := range args {
+		fmt.Print(s + " ")
+	}
+	fmt.Print("\n")
+}
+
+func exit(args []string) {
+
+	if len(args) < 1 {
+		fmt.Println("Not enough arguments!")
+		os.Exit(1)
+	}
+	exitCode, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	os.Exit(exitCode)
 }
